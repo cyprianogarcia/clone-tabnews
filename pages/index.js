180e36777285f066d984353b3861c5b2cec6dfb7
cyprianogarcia <cyprianogarcia@gmail.com>
2023-08-09 20:48:51 +0000

    uma imagem vale mais que mil palavras

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home(){
-    return <h1>Hello Cypriano</h1>;
-}
+    content = '<div style="width:100vw;height:100vh;display:flex;flex-direction:column;align-items:center;justify-content:center;gap:20px"><p>Topa fazer uma coisa ilegal?</p><img src="https://i.postimg.cc/mZJkgkXD/ipilife.jpg"></div>';
+    return content;
+};
 
 export default Home;
